243203184bac8040499189736fbe3a2c607a873b
arjuntahir <veginativenky124@gmail.com>
2023-09-15 09:16:14 -0700

    Inheritance

---
+++ src/inheritance/Singlelevel_inheritance.java
@@ -0,0 +1,24 @@
+package inheritance;
+
+public class Singlelevel_inheritance {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Cricket & Football");
+        Cricket C = new Cricket();
+        C.display();
+        C.display1();
+	}
+}
+	class Football{
+		public void display() {
+			System.out.println(" Cristiano ronaldo");
+		}
+		
+	}
+	class Cricket extends Football {
+		public void display1() {
+			System.out.println("Virat Kohli");
+		}
+        
+	}
+
